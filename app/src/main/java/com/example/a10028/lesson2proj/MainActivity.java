00a02e04d076a0d0e01c8194f44f0442698749d8
ququ307 <1002849307@qq.com>
2018-05-20 20:08:38 +0800

    1：新增了一个activity。

--- app/src/main/java/com/example/a10028/lesson2proj/MainActivity.java
+++ app/src/main/java/com/example/a10028/lesson2proj/MainActivity.java
@@ -1,18 +1,99 @@
 package com.example.a10028.lesson2proj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-   private static  final String TGA ="MainActivity";
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"you clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Remove_item:
+                Toast.makeText(this,"you clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+         return  true;
+    }
+
+    private  String TAG = "hello";
+    private TextView btn_center;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Log.d("MainActivity","onCreate execute");
-        Log.d("MainActivity", "onCreate: execute");
+        Button button1=(Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+                                       @Override
+                                       public void onClick(View view) {
+                                           Toast.makeText(MainActivity.this,"世界那么大，何时去看！",Toast.LENGTH_SHORT).show();
+                                           Log.i(TAG, "had been onClick 西方");
+                                       }
+                                   });
+        Button button2=(Button)findViewById(R.id.button2) ;
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("com.example.a10028.lesson2proj.ACTION_1");
+                startActivity(intent);
+                Log.i(TAG, "had been onClick 北方");
+            }
+        });
+        Button button3=(Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"世界那么大，我找到了你！",Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "had been onClick  东方");
+            }
+        });
+        Button button4=(Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent("com.example.a10028.lesson2proj.ACTION");
+                startActivity(intent);
+                Log.i(TAG, "had been onClick  南方");
+            }
+        });
+
+
+        Log.i(TAG, "start a click event");
+        btn_center= (TextView) findViewById(R.id.btn_center);
+        btn_center.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent("com.example.a10028.lesson2proj.ACTION_START");
+                startActivity(intent);
+                Log.i(TAG, "had been onClick 世界中心");
+            }
+
+
+        });
+
+
+
+
     }
 }
 
